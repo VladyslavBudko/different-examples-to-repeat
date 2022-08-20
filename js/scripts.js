8351d96d0b3e0c824d3cd6f3d88d82eec5d4ff3f
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-20 19:30:47 +0300

    5

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,13 @@
 
 
 
+// 5
+
+document.addEventListener("keydown", event => {
+    event.preventDefault();
+  
+    if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
+      console.log("«Ctrl + s» or «Command + s» combo");
+    }
+  });
+  
